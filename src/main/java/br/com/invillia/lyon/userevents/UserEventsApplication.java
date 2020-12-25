718632acd6c08caff3bc7d8edab30003f55ca573
Leonardo Luis Domingos <you@example.com>
2020-12-25 15:22:04 -0300

    feature/LYON-02

--- src/main/java/br/com/invillia/lyon/userevents/UserEventsApplication.java
+++ src/main/java/br/com/invillia/lyon/userevents/UserEventsApplication.java
@@ -1,9 +1,14 @@
 package br.com.invillia.lyon.userevents;
 
+import br.com.invillia.lyon.userevents.channel.UserEventsChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding({
+		UserEventsChannel.class
+})
 public class UserEventsApplication {
 
 	public static void main(String[] args) {
