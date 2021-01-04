1e326873e48de19b884910ddeb6eb555b9dd393b
Leonardo Luis Domingos <you@example.com>
2021-01-04 07:45:40 -0300

    feature/LYON-02

--- src/main/java/br/com/invillia/lyon/userevents/service/SwapiService.java
+++ src/main/java/br/com/invillia/lyon/userevents/service/SwapiService.java
@@ -4,14 +4,17 @@ import br.com.invillia.lyon.userevents.Repository.PersonRepository;
 import br.com.invillia.lyon.userevents.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
 public class SwapiService {
 
-    private final String BASE_URL = "https://swapi.dev/api/people/";
+    //@Value("${spring.custom.api-url}")
+    public static String BASE_URL = "https://swapi.dev/api/people/";
 
     private RestTemplate restTemplate = new RestTemplate();
 
@@ -20,11 +23,17 @@ public class SwapiService {
 
     public void findPerson(String id) {
         String url = BASE_URL + id + "/";
-        User user = restTemplate
-                .getForObject(url, User.class);
+        try {
+            User user = restTemplate
+                    .getForObject(url, User.class);
 
-        log.info("M=findPerson, I=acessando a api,");
+            log.info("M=findPerson, I=acessando a api,");
 
-        personRepository.save(user);
+            personRepository.save(user);
+        }catch (HttpClientErrorException h) {
+            if(h.getStatusCode().equals("404")) log.info("personagem não encontrado");
+        }catch (Exception e) {
+            log.info("serviço fora");
+        }
     }
 }
