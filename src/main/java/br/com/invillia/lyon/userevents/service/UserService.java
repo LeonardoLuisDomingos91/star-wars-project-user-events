c661ca9dd590fadd03aba3e7d91ec81497c4d420
Leonardo Luis Domingos <you@example.com>
2021-01-21 14:02:16 -0300

    LYON-281 teste

--- src/main/java/br/com/invillia/lyon/userevents/service/UserService.java
+++ src/main/java/br/com/invillia/lyon/userevents/service/UserService.java
@@ -8,8 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
-
 @Service
 @Slf4j
 @AllArgsConstructor
